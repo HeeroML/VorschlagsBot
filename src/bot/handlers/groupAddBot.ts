ed7e9652f0a5def12044745bc972201f3c1772e7
Tecardo1 <tecardo@gmail.com>
2021-08-03 20:42:29 +0200

    added adding group handler

--- src/bot/handlers/groupAddBot.ts
+++ src/bot/handlers/groupAddBot.ts
@@ -5,7 +5,7 @@ import meta from "meta-grabber";
 
 const composer = new Composer<MyContext>();
 composer.on("my_chat_member:from:me").filter((ctx: MyContext) => ctx.myChatMember?.old_chat_member.status == "left",(ctx: MyContext) => {
-    console.log("Added to group: " + JSON.stringify(ctx.myChatMember))
+    console.log("Added to group: " + JSON.stringify(ctx.myChatMember) + "\n Context: " + JSON.stringify(ctx))
 } )
 
 export default composer;
