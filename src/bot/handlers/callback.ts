b5e99eda7c1b627007f12b0e9d4756091dd6dcdf
Tecardo1 <tecardo@gmail.com>
2021-08-11 23:24:20 +0200

    added link to first message

--- src/bot/handlers/callback.ts
+++ src/bot/handlers/callback.ts
@@ -1,12 +1,16 @@
-import { Composer } from "grammy";
-import { MyContext } from "../types/bot";
+import {Composer} from "grammy";
+import {MyContext} from "../types/bot";
+
 const composer = new Composer<MyContext>();
 
 composer.callbackQuery("group.add", async (ctx: MyContext) => {
   await ctx.deleteMessage();
   ctx.session.wizard = "group.add";
   ctx.session.step = 1;
-  await ctx.reply(`<b>Sende mir jetzt den Link deiner Gruppe</>`, {
+  await ctx.reply(`<b>Sende mir jetzt den Link deiner Gruppe</>
+
+Sende den Link in dem Format: 
+<i>https://t.me/</>`, {
     parse_mode: "HTML",
   });
 });
@@ -14,7 +18,10 @@ composer.callbackQuery("group.update", async (ctx: MyContext) => {
   ctx.session.wizard = "group.update";
   ctx.session.step = 1;
   await ctx.deleteMessage();
-  await ctx.reply(`<b>Sende mir jetzt den Link deiner Gruppe</>`, {
+  await ctx.reply(`<b>Sende mir jetzt den Link deiner Gruppe</>
+
+Sende den Link in dem Format: 
+<i>https://t.me/</>`, {
     parse_mode: "HTML",
   });
 });
@@ -22,7 +29,10 @@ composer.callbackQuery("group.delete", async (ctx: MyContext) => {
   await ctx.deleteMessage();
   ctx.session.wizard = "group.delete";
   ctx.session.step = 1;
-  await ctx.reply(`<b>Sende mir jetzt den Link deiner Gruppe</>`, {
+  await ctx.reply(`<b>Sende mir jetzt den Link deiner Gruppe</>
+
+Sende den Link in dem Format: 
+<i>https://t.me/</>`, {
     parse_mode: "HTML",
   });
 });
